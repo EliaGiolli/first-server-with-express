f9f5c4084a1ac7e6345befcbd397a88912c3ea12
Elia Giolli <eliagiolli22@gmail.com>
2025-08-08 21:05:35 +0200

    removed mysql and added mongoDB, added helmet for security and refactored index and server

---
+++ src/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+
+import quotesRoutes from './routes/quotes.js';
+import authRoutes from './routes/auth.js';
+import helmet from 'helmet';
+
+
+const app = express();
+
+app.use(express.static('Public'));
+
+app.set('view engine','ejs')
+
+// MIDDLEWARES
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+//Helmet helps the app to be protected from vulnerabilities
+app.use(helmet());
+
+//ROUTES
+app.use('/', authRoutes)
+app.use('/api/quotes', quotesRoutes)
+app.use('/', authRoutes)
+app.use('/api/quotes', quotesRoutes)
+
+app.use((err, _req, res, _next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).json({ error: err.message || 'Internal Server Error' });
+});
+
+
+export default app;
